68907bb0cb5d97f03582e354508141c6bcfddaae
aspowerr <31863149+aspowerr@users.noreply.github.com>
2018-09-23 14:13:53 +0300

    18 passing -comparator

--- src/index.js
+++ src/index.js
@@ -32,27 +32,51 @@ class Sorter {
   sort(indices) {
     // your implementation
     let q, endQ, endW, w;
-
     let new_array = [];
+    
+    for (q = 0, endQ = indices.length - 1; q < endQ; q++)
+    {
+      for(w = 0, endW = indices.length - q; w < endW; w++)
+      {
+        if(indices[w] > indices[w+1])
+          {
+            var swap = indices[w];
+
+            indices[w] = indices[w+1];
+            indices[w+1] = swap;
+
+          }
+      }
+    }
+
     for(let j=0; j<indices.length; j++)
     {
-    new_array[j] = array[indices[j]];
+      new_array[j] = array[indices[j]];
     }
 
     for (q = 0, endQ = new_array.length - 1; q < endQ; q++)
     {
-      let wasSwap = false;
-
-      for(w = 0, endW = endW - q; w < endW; w++)
+      for(w = 0, endW = new_array.length - q; w < endW; w++)
       {
-        if(new_array[w] > new_array[w+1]) {
-          [new_array[w], new_array[w+1]] = [new_array[w+1], new_array[w]];
-          wasSwap = true;
-        }
-        if(!wasSwap) break;
+        if(new_array[w] > new_array[w+1])
+          {
+            var swap = new_array[w];
+
+            new_array[w] = new_array[w+1];
+            new_array[w+1] = swap;
+
+          }
       }
-      return new_array;
     }
+
+    w=0;
+    for(q=0; q<indices.length; q++)
+    {
+        array[indices[q]]=new_array[w];
+        w++;
+    }
+
+  return array;
   }
 
   setComparator(compareFunction) {
